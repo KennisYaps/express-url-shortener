102b6a5074b480f48121cc0c0a7f759997c57a96
yyx <yuan_xin96@hotmail.com>
2018-03-14 14:34:03 +0800

    all pass

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.post("/shorten-url", function(req, res) {
   var encoding = encode(getURL, existingURLs);
   var matchingURLs = existingURLs.filter(element => element.url === getURL);
   if (matchingURLs.length === 0) {
-    existingURLs.push(generateHash);
+    var newObject = {
+      id: Number.parseInt(existingURLs.length - 1) + 1,
+      url: getURL,
+      hash: encoding
+    };
+    existingURLs.push(newObject);
   }
   res.send({
     hash: encoding
@@ -47,26 +52,27 @@ app.get("/expand-url/:hash", function(req, res) {
 });
 
 // TASK 3: DELETE/expand-url/:hash
-app.delete("/expand-url/:hash",function(req,res){
-  try{    
-    var getHash = req.params.hash; 
-    let matchingHash = existingURLs.filter(object => {
+app.delete("/expand-url/:hash", function(req, res) {
+  var getHash = req.params.hash;
+  let matchingHash = existingURLs.filter(
+    object =>
       // you need to separate in order to debug what is object.hash
-        object.hash === getHash});
-    if (matchingHash.length === 1 ){
-      existingURLs.splice(0,1);
-      res.status(200); 
-      res.send(`{
+      object.hash === getHash
+  );
+  if (matchingHash.length === 1) {
+    existingURLs.splice(0, 1);
+    res.status(200);
+    res.send(`{
           "message": "URL with hash value ${getHash} deleted successfully";
-      }`)
-    } 
-  }catch(error){
+      }`);
+  } else {
     res.status(404);
     res.send(`{
       "message": "URL with hash value ${getHash} does not exist";
-  }`)
+  }`);
   }
-})
+});
+
 // this read the error (404) and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error("Not Found");
@@ -74,7 +80,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler 
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
